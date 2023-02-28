53d393955891ad18bdca343b00efdd425043d375
Nhuengzii <32nhueng@gmail.com>
2023-02-28 23:03:58 +0700

    Big mess commit

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+from math import prod
+import torch
 import torch.nn as nn
 from torch.nn import ConvTranspose2d, BatchNorm2d, ReLU, LeakyReLU, Conv2d, Sigmoid
 
@@ -98,3 +100,70 @@ class DiscriminatorNetwork2(nn.Module):
     def forward(self, x):
         x = self.main(x)
         return self.last_layer(x)
+class Generator128Network(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        def ConvTransposeBlock(in_c, out_c, kernel_size, stride, padding):
+            return nn.Sequential(
+                    ConvTranspose2d(in_c, out_c, kernel_size, stride, padding),
+                    BatchNorm2d(out_c),
+                    ReLU(True)
+                    )
+        self.main = nn.Sequential(
+                # Input (128, 1, 1)
+                ConvTranspose2d(128,1024, 4, 1, 0),
+                ReLU(True),
+
+                # (1024, 4, 4)
+                ConvTransposeBlock(1024, 512, 4, 2, 1),
+                # (512, 8, 8)
+                ConvTransposeBlock(512, 256, 4, 2, 1),
+                # (256, 16, 16)
+                ConvTransposeBlock(256, 128, 4, 2, 1),
+                # (128, 32, 32)
+                ConvTransposeBlock(128, 64, 4, 2, 1),
+                # (64, 32, 32)
+                ConvTransposeBlock(64, 3, 4, 2, 1),
+                # (32, 64, 64)
+                # (32, 128, 128)
+                nn.Tanh(),
+                )
+    def forward(self, x):
+        x = self.main(x)
+        return x
+
+class Discriminator128Network(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        def ConvBlock(in_c, out_c, kernel_size, stride, padding):
+            return nn.Sequential(
+                    Conv2d(in_c, out_c, kernel_size, stride, padding, bias=False),
+                    BatchNorm2d(out_c),
+                    LeakyReLU(0.2, True)
+                    )
+        self.feature_extractor = nn.Sequential(
+                    # (3, 128, 128)
+                    Conv2d(3, 32, 4, 2, 1, bias=False),
+                    LeakyReLU(0.2, True),
+
+                    # (32, 64, 64)
+                    ConvBlock(32, 64, 4, 2, 1),
+                    # (64, 32, 32)
+                    ConvBlock(64, 128, 4, 2, 1),
+                    # (128, 16, 16)
+                    ConvBlock(128, 256, 4, 2, 1),
+                    # (256, 8, 8)
+                    ConvBlock(256, 512, 4, 2, 1)
+                    # (512, 4, 4)
+                )
+        self.classifier = nn.Sequential(
+                    nn.Flatten(),
+                    nn.Linear(512 * 4 * 4, 1024),
+                    nn.LeakyReLU(0.2, True),
+                    nn.Linear(1024, 1),
+                    nn.Sigmoid()
+                )
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        return self.classifier(x)
+        
