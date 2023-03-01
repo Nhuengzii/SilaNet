42bca3746da6bef797be0e8c763efaa4c8ed5b9b
Nhuengzii <32nhuengzii@gmail.com>
2023-03-01 20:04:16 +0700

    fix load_state_dict erro when run on non cuda device

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ matplotlib.use("Agg")
 import numpy as np
 
 # Load your model
+device = torch.device("cpu")
 generator = GeneratorNetwork()
-generator.load_state_dict(torch.load("./checkpoint/generator_sila_1000.pt"))
+generator.load_state_dict(torch.load("./checkpoint/generator_sila_1000.pt", map_location=device))
+generator = generator.to(device)
 
 # Define the function to generate images with random noise
 
 def generate_image(s):
-    fixed_noise = torch.randn((4, 100, 1, 1))
+    fixed_noise = torch.randn((4, 100, 1, 1)).to(device)
     noise = fixed_noise + s
     with torch.no_grad():
         images = generator(noise)
